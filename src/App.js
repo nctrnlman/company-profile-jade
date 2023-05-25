b3cb7fdc04faa6f8fd868ff26b1ffeefbbeb3504
Rhazes Devino <rhazesd@gmail.com>
2023-05-25 22:37:40 +0700

    add home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="bg-black">
+      <h1 className="text-white">HOME</h1>
+    </div>
+  );
+}
+
+export default Home;
