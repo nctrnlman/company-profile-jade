58f3580579967ddaba0ddfe14ea3793605f9e11c
Khaled Makkawirelang <121877660+oledunwahid@users.noreply.github.com>
2023-05-27 02:37:04 +0700

    add-responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: '280px',
+      md: '480px',
+      lg: '960px',
+      xl: '1200px',
+    },
     extend: {},
   },
   plugins: [],
