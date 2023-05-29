2020effb587230fe75a8cad417f4d1f665d6bf59
Rhazes Devino <rhazesd@gmail.com>
2023-05-30 05:28:37 +0700

    add-navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Service from "./pages/Service";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/service" element={<Service />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../assets/logo_jade_tech.png";
+import { useNavigate } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="flex justify-between m-auto">
-      <div>
-        <h1>LOGO</h1>
+    <nav className="bg-[#1190A6] overflow-x-hidden ">
+      <div className="flex justify-between m-auto pt-4 items-center flex-wrap w-full  fixed lg:px-[5%] ">
+        <div className="pb-4 pl-5 pt-2 lg:pt-6">
+          <img src={logo} alt="logo perusahaan" width="150px" className="" />
+        </div>
+
+        <GiHamburgerMenu
+          className="text-white  lg:hidden block cursor-pointer text-5xl mr-5 hover:bg-black/10 h-15 w-auto p-2 rounded-2xl "
+          onClick={() => setOpen(!open)}
+        />
+
+        {!open ? (
+          <div
+            className={`${
+              open ? "block" : "hidden"
+            } lg:flex lg:gap-7 w-full lg:w-auto text-white  bg-black/20 lg:bg-transparent `}
+          >
+            <ul className="lg:flex lg:justify-between lg:gap-7 text-lg lg:text-2xl ">
+              <li className="hover:bg-black/10 block  py-2 cursor-pointer lg:hover:bg-transparent pl-2">
+                <button
+                  className="lg:hover:text-[#004c9b]"
+                  onClick={() => navigate("/")}
+                >
+                  Profile
+                </button>
+              </li>
+              <li className="hover:bg-black/10 block  py-2 cursor-pointer lg:hover:bg-transparent pl-2">
+                <button
+                  className="lg:hover:text-[#004c9b]"
+                  onClick={() => navigate("/service")}
+                >
+                  Service
+                </button>
+              </li>
+              <li className="hover:bg-black/10 block  py-2 cursor-pointer lg:hover:bg-transparent pl-2">
+                <button className="lg:hover:text-[#004c9b]">Contact</button>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <motion.div
+            variants={fadeIn("down", 0.1)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.1 }}
+            className={`${
+              open ? "block" : "hidden"
+            } lg:flex lg:gap-7 w-full lg:w-auto text-white  bg-black/20 lg:bg-transparent `}
+          >
+            <ul className="lg:flex lg:justify-between lg:gap-7 text-lg lg:text-2xl ">
+              <li className="hover:bg-black/10 block  py-2 cursor-pointer lg:hover:bg-transparent pl-2">
+                <button
+                  className="lg:hover:text-[#004c9b]"
+                  onClick={() => navigate("/")}
+                >
+                  Profile
+                </button>
+              </li>
+              <li className="hover:bg-black/10 block  py-2 cursor-pointer lg:hover:bg-transparent pl-2">
+                <button
+                  className="lg:hover:text-[#004c9b]"
+                  onClick={() => navigate("/service")}
+                >
+                  Service
+                </button>
+              </li>
+              <li className="hover:bg-black/10 block  py-2 cursor-pointer lg:hover:bg-transparent pl-2">
+                <button className="lg:hover:text-[#004c9b]">Contact</button>
+              </li>
+            </ul>
+          </motion.div>
+        )}
       </div>
-      <div>
-        <button>Home</button>
-        <button>About</button>
-        <button>Project</button>
-        <button>Purpose</button>
-      </div>
-    </div>
+    </nav>
   );
 }
 
