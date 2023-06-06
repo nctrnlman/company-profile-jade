c8dab24f59beb3ef17c3001397c57fab1f5162d5
Khaled Makkawirelang <121877660+oledunwahid@users.noreply.github.com>
2023-06-06 10:50:30 +0700

    Update Service.jsx

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -5,12 +5,12 @@ function Service() {
     <section className="min-h-[85vh] lg:min-h-[90vh] h-[700px] flex items-center overflow-x-hidden bg-[#1190A6]">
       <div className="container justify-between flex flex-col lg:flex-row lg:items-center lg:gap-x-12">
         {/*text */}
-        <div className="flex flex-col px-[15px] mx-5 text-center lg:text-left gap-y-3">
+        <div className="flex flex-col px-5 mx-5 text-center lg:text-left gap-y-3">
           <h1 className="text-[76px] font-bold leading-[1] lg:text[100px] text-white">
-            Jade <span> Technology </span>{" "}
+            Our <span> Specialty </span>{" "}
           </h1>
           <p className="text-[#004c9b] text-[31px] font-semibold">
-            EdsaaddddddDigital Transformation
+          Supporting your company with all it's digital needs.
           </p>
         </div>
 
