4cae9b6730e87a1c909990bafc3ed149dce6d37b
Khaled Makkawirelang <121877660+oledunwahid@users.noreply.github.com>
2023-06-10 14:52:41 +0700

    repair responsive

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -16,7 +16,7 @@ function Service() {
         whileInView={"show"}
         viewport={{ once: false, amount: 0.3 }}
         className="flex flex-col px-5 mx-5 text-center lg:text-left gap-y-3">
-          <h1 className="text-[76px] font-bold leading-[1] lg:text[100px] text-white">
+          <h1 className="text-[60px] font-bold leading-[1] lg:text-[80px] text-white">
             Our <span> Specialty </span>{" "}
           </h1>
           <motion.p
@@ -55,62 +55,94 @@ function Service() {
 
         {/* card */}
         
-        <div className=" px-2 flex flex-col lg:flex lg:flex-row overflow-hidden max-w-lg lg:h-[350px] mx-auto lg:mx-5 h-[1000px] items-center rounded-xl bg-[#1190A6] lg:max-w-full py-12 lg:py-12">
-          <div className="bg-slate-200 h-[250px] w-[250px] mx-5 px-5 border-2
-          border-y-blue-900 border-x-blue-800 rounded-xl py-4">
-            <h1 className="py-2 font-semibold text-lg text-[#004c9b]"> Website <span/> Design </h1>
-            <div className="text-sm">
-            <p> We don't build the most beautiful websites in the world. </p>
-            <p>We build websites that convert.</p>  
-            </div>
-          </div>
-        
-        {/* card 2 */}
-        <div className="py-2">
-          <div className="bg-slate-200 h-[250px] w-[250px] mx-5 px-5 border-2
-          border-y-blue-900 border-x-blue-800 rounded-xl py-4">
-            <h1 className="py-2 font-semibold text-lg text-[#004c9b]"> Web <span/> Development </h1>
-           <div className="text-sm">
-            <p>In the dynamic digital landscape, web development plays a pivotal role in shaping businesses' online presence. </p>  
-            </div> 
-          </div>
-          </div>
-
-          {/* img */}
-          <div className="flex">
-            <img src={Image3} alt=""/>
-          </div>
-
-
-          {/* card 3 */}
-         
-          <div className="bg-slate-200 h-[250px] w-[250px] mx-5 px-5 border-2
-          border-y-blue-900 border-x-blue-800 rounded-xl py-4 ">
-            <h1 className="py-2 font-semibold text-lg text-[#004c9b]"> Mobile <span/> Design </h1>
-            {/* div p */}
-            <div className="text-sm">
-              <p> In the mobile-centric world we live in, having a captivating mobile design is essential.</p> 
-            </div>
-
-          </div>
-          {/* card 4 */}
-          <div className="py-2">
-          <div className="bg-slate-200 h-[250px] w-[250px] mx-5 px-5 border-2
-          border-y-blue-900 border-x-blue-800 rounded-xl py-4">
-            <h1 className="py-2 font-semibold text-lg text-[#004c9b]"> Mobile <span/> Development </h1>
-            {/* div p */}
-            <div className="text-sm">
-              <p> We excel in mobile development, harnessing the power of cutting-edge technologies to create innovative and user-centric mobile applications. </p> 
-            </div>
-            </div>
-          </div>
+        <motion.div 
+         variants={fadeIn("fade", 0.3)}
+         initial="hidden"
+         whileInView={"show"}
+         viewport={{ once: false, amount: 0.3 }}
+        className=" px-2 flex flex-col lg:flex lg:flex-row overflow-hidden max-w-lg lg:h-[350px] mx-auto lg:mx-5 h-[580px] items-center rounded-xl bg-[#1190A6] lg:max-w-full py-12 lg:py-12">
           
-         
-          
-         
-        </div>
+        <motion.div 
+         variants={fadeIn("right", 0.5)}
+         initial="hidden"
+         whileInView={"show"}
+         viewport={{ once: false, amount: 0.3 }}
+        className="py-2">
+            <div className="bg-slate-200 lg:h-[250px] lg:w-[250px] mx-5 px-5 border-2
+              border-y-blue-900 border-x-blue-800 rounded-xl py-2">
+              <h1 className="py-2 font-semibold text-lg text-[#004c9b]"> Web <span/> Design </h1>
+            <div className="text-sm">
+            <p> We don't build the most beautiful websites in the world.
+                We build websites that convert.
+            </p>  
+              </div> 
+            </div>
+            </motion.div>
         
+          {/* card 2 */}
+          <motion.div 
+           variants={fadeIn("right", 0.3)}
+           initial="hidden"
+           whileInView={"show"}
+           viewport={{ once: false, amount: 0.3 }}
+          className="py-2">
+            <div className="bg-slate-200 lg:h-[250px] lg:w-[250px] mx-5 px-5 border-2
+              border-y-blue-900 border-x-blue-800 rounded-xl py-2">
+              <h1 className="py-2 font-semibold text-lg text-[#004c9b]"> Web <span/> Development </h1>
+            <div className="text-sm">
+              <p>In the dynamic digital landscape, web development plays a pivotal role in shaping businesses' online presence. </p>  
+              </div> 
+            </div>
+            </motion.div>
 
+            {/* img */}
+            <motion.div 
+             variants={fadeIn("fade", 0.3)}
+             initial="hidden"
+             whileInView={"show"}
+             viewport={{ once: false, amount: 0.3 }}
+            className="hidden px-[30px] lg:flex flex-1 ">
+              <img src={Image3} alt=""/>
+            </motion.div>
+
+
+            {/* card 3 */}
+          <motion.div 
+           variants={fadeIn("left", 0.3)}
+           initial="hidden"
+           whileInView={"show"}
+           viewport={{ once: false, amount: 0.3 }}
+          className="py-2">
+            <div className="bg-slate-200 lg:h-[250px] lg:w-[250px] mx-5 px-5 border-2
+              border-y-blue-900 border-x-blue-800 rounded-xl py-2 ">
+              <h1 className="py-2 font-semibold text-lg text-[#004c9b]"> Mobile <span/> Design </h1>
+              {/* div p */}
+              <div className="text-sm">
+                <p> In the mobile-centric world we live in, having a captivating mobile design is essential.</p> 
+              </div>
+              </div>
+            </motion.div>
+
+            {/* card 4 */}
+            <motion.div 
+             variants={fadeIn("left", 0.5)}
+             initial="hidden"
+             whileInView={"show"}
+             viewport={{ once: false, amount: 0.3 }}
+            className="py-2">
+            <div className="bg-slate-200 lg:h-[250px] lg:w-[250px] mx-5 px-5 border-2
+              border-y-blue-900 border-x-blue-800 rounded-xl py-2">
+              <h1 className="py-2 font-semibold text-lg text-[#004c9b]"> Mobile <span/> Development </h1>
+              {/* div p */}
+              <div className="text-sm">
+                <p> We excel in mobile development, harnessing the power of cutting-edge technologiesss. </p> 
+              </div>
+              </div>
+            </motion.div>
+                   
+         
+        </motion.div>
+        
       </div>
     </section>
     </>
